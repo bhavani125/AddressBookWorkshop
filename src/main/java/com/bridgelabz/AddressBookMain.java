bdedd011ac2133743fef9c47f76c378e64b4c4e8
bhavani125 <girinenibhavani12@gmil.com>
2021-08-18 08:47:48 +0530

    Added:code of creating conntacts

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 public class AddressBookMain {
+    //Main method
     public static void main(String[] args) {
         System.out.println("Welcome to address book service");
     }
